accf42cdd1efd1ab8ee790badddde422578359fb
Nilanchal Panigrahy <nilanchal.panigrahy@tribalworldwide.co.uk>
2023-10-18 12:02:57 +0100

    Consolidated all UI examples into single repo

---
+++ Android-RadioButton-Example/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* Initialize Radio Group and attach click handler */
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.clearCheck();
+
+        /* Attach CheckedChangeListener to radio group */
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                if(null!=rb && checkedId > -1){
+                    Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    public void onClear(View v) {
+        /* Clears all selected radio buttons to default */
+        radioGroup.clearCheck();
+    }
+
+    public void onSubmit(View v) {
+        RadioButton rb = (RadioButton) radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
+        Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+    }
+}
